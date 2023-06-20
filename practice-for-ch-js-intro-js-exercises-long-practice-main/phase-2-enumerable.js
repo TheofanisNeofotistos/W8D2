0dc6e81c1b8e98ec88121eb2bd5fe4ef1373a902
Michelle <rphmichelle@gmail.com>
2023-06-20 17:31:35 -0400

    working on phase 2

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,33 @@
-// Your code here
+Array.prototype.myEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i]);
+    }
+};
+
+Array.prototype.myMap = function (callback) {
+    newArr = [];
+    this.myEach(ele => {
+        newArr.push(callback(ele))
+    })
+    return newArr;
+};
+
+
+// let a = [1, 2, 3]
+
+// const callback = function (el) {
+//     return el * el;
+// }
+
+// console.log(a.myMap(callback));
+
+
+Array.prototype.myReduce = function (callback) {
+    let sum = 0
+    this.myEach(ele => {
+        sum += element
+    })
+    return sum + initialValue
+}
+
+const callback = function (el)
